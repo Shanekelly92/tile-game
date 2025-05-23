464d86a3a2b619fc2617fd047853d5093b85af1f
Shanekelly92 <shane.kelly@bbc.co.uk>
2025-05-23 16:25:38 +0100

    code is now passing tests

--- src/commonMain/kotlin/NewLetterGenerator.kt
+++ src/commonMain/kotlin/NewLetterGenerator.kt
@@ -1,33 +1,31 @@
-class NewLetterGenerator(val dictionary: Set<String>,  maxTilesInRack: Int = 7) {
+class NewLetterGenerator(val dictionary: Set<String>,  val maxTilesInRack: Int = 7) {
 
-    fun findViableNextLetters(currentWord: String, currentTiles: Set<Char>): Set<Char>? {
+    fun findViableNextLetters(currentWord: String, currentTiles: List<Char>): List<Char>? {
 
         if(!dictionary.contains(currentWord)) {
-            throw IllegalStateException("The current word should always be in the dictionary as it's a previsously validated word!");
+            throw IllegalStateException("The current word should always be in the dictionary as it's a previously validated word!");
         }
-
         var subStrings : List<String>? = null
         for (word in dictionary){
+            if (word.equals(currentWord)) continue
             if (word.contains(currentWord)) {
                 subStrings = word.split(currentWord)
-                break
+
+                val requiredLetters = run {
+                    val letters = ArrayList<Char>()
+                    for (substring in subStrings) {
+                        letters.addAll(substring.toSet())
+                    }
+                    letters // for some reason no need for return statement, a bit like ruby in this context
+                }
+                requiredLetters.removeAll(requiredLetters.intersect(currentTiles))
+                if (requiredLetters.size + currentTiles.size > maxTilesInRack) continue
+                println("new possible word $word")
+                println("required letters ${requiredLetters.toString()}")
+                return requiredLetters
             }
         }
-
-        subStrings ?: return null
-
-        val requiredLetters = run {
-            val letters = HashSet<Char>()
-            for (substring in subStrings) {
-                letters.addAll(substring.toSet())
-            }
-            letters // for some reason no need for return statement, a bit like ruby in this context
-        }
-        //TODO ooooooohhhhh I just realised the fact I am using Set means duplicate letters are not considered!
-
-        requiredLetters.removeAll(requiredLetters.intersect(currentTiles)) // I think this should return all the letters that aren't in the currentTiles
-
-        return requiredLetters
+        return null // null means no possible new word can be made from currentWord //todo: not great to use null as return value
     }
 
 }
--- src/commonTest/kotlin/test.kt
+++ src/commonTest/kotlin/test.kt
@@ -11,11 +11,15 @@ class MyTest : ViewsForTesting() {
 
 
     val testWords = setOf(
-
+        "free",
+        "id",
+        "idea",
+        "ideas",
+        "freedom",
         "doorway",
+        "door",
         "do",
         "undo",
-        "freedom",
         "cat",
         "upside",
         "blah",
@@ -34,7 +38,7 @@ class MyTest : ViewsForTesting() {
      */
     fun testGetNewViableTile_noPossibleNewTile_null(){
         val currentWord = "door"
-        val currentTiles = setOf('x','y', 'v', 'a', 'b', 'x')
+        val currentTiles = listOf('x','x', 'v', 'a', 'b', 'x')
         val output = underTest.findViableNextLetters(currentWord, currentTiles)
         assertNull(output)
     }
@@ -42,7 +46,7 @@ class MyTest : ViewsForTesting() {
     @Test
     fun testGetNewViableTile_wordExtendPOSTfix_currentTilesEnough_noNewTiles(){
         val currentWord = "door"
-        val currentTiles = setOf('y','a', 'w') //w a y already present to make doorway
+        val currentTiles = listOf('y','a', 'w') //w a y already present to make doorway
         val output = underTest.findViableNextLetters(currentWord, currentTiles)
         assertEquals(0, output?.size)
     }
@@ -50,7 +54,7 @@ class MyTest : ViewsForTesting() {
     @Test
     fun testGetNewViableTile_wordExtendPOSTfix_needTwoAdditionalTiles_twoNewTiles(){
         val currentWord = "door"
-        val currentTiles = setOf('y','b', 'z') // requires w and a to make doorway
+        val currentTiles = listOf('y','b', 'z') // requires w and a to make doorway
         val output = underTest.findViableNextLetters(currentWord, currentTiles)
         assertEquals(2, output?.size)
         assertTrue(output!!.contains('w'))
@@ -60,45 +64,53 @@ class MyTest : ViewsForTesting() {
 
     @Test
     fun testGetNewViableTile_wordExtendSUFFix_currentTilesEnough_noNewTiles(){
-        val currentWord = "do"
-        val currentTiles = setOf('n','f', 'u', 't', 't') // u n already present to make undo. Not enough space to make doorway
+        val currentWord = "id" //lets try to make idea
+        val currentTiles = listOf('n','a', 'u', 'e', 't') // e and a present
         val output = underTest.findViableNextLetters(currentWord, currentTiles)
         assertEquals(0, output?.size)
     }
 
     @Test
+    fun testGetNewViableTile_wordExtendSUFFix_needsNewTile_noSpace_null(){
+        val currentWord = "idea" //lets try to make ideas
+        val currentTiles = listOf('n','a', 'u', 'e', 't', 'x', 'x') // e and a present
+        val output = underTest.findViableNextLetters(currentWord, currentTiles)
+        assertNull(output)
+    }
+
+    @Test
     fun testGetNewViableTile_wordExtendSUFFix_needTwoAdditionalTiles_twoNewTiles(){
-        val currentWord = "do"
-        val currentTiles = setOf('b','f', 'u', 't', 't') // u n already present to make undo. Not enough space to make doorway
+        val currentWord = "id" // lets try to make idea
+        val currentTiles = listOf('b','x', 'u', 't', 't') //
         val output = underTest.findViableNextLetters(currentWord, currentTiles)
         assertEquals(2, output?.size)
-        assertTrue(output!!.contains('n'))
-        assertTrue(output!!.contains('u'))
+        assertTrue(output!!.contains('e'))
+        assertTrue(output!!.contains('a'))
     }
 
     @Test
     fun testGetNewViableTile_wordExtendBOTHWAYS_currentTilesEnough_noNewTiles(){
         val currentWord = "do"
-        val currentTiles = setOf('f','r', 'e', 'e', 'm', 'x') // tiles can make 'freedom' Not enough space to make 'do' or 'doorway'
+        val currentTiles = listOf('f','r', 'e', 'e', 'm', 'x') // tiles can make 'freedom' Not enough space to make 'do' or 'doorway'
         val output = underTest.findViableNextLetters(currentWord, currentTiles)
         assertEquals(0, output?.size)
     }
 
     @Test
     fun testGetNewViableTile_wordExtendBOTHWAYS_needThreeAdditionalTiles_threeNewTiles(){
-        val currentWord = "do"
-        val currentTiles = setOf('f','x', 'm', 'x') // tiles can make 'freedom' Not enough space to make 'do' or 'doorway'
+        val currentWord = "free"
+        val currentTiles = listOf('f','x', 'x', 'x') // tiles can make 'freedom' Not enough space to make 'do' or 'doorway'
         val output = underTest.findViableNextLetters(currentWord, currentTiles)
         assertEquals(3, output?.size)
-        assertTrue(output!!.contains('r'))
-        assertTrue(output!!.contains('e'))
-        assertTrue(output!!.contains('e'))
+        assertTrue(output!!.contains('d'))
+        assertTrue(output!!.contains('o'))
+        assertTrue(output!!.contains('m'))
     }
 
     @Test
     fun testGetNewViableTile_wordExtendBOTHWAYS_needThreeAdditionalTiles_NOT_ENOUGH_SPACE_Null(){
         val currentWord = "do"
-        val currentTiles = setOf('f','x', 'x', 'x', 'm', 'x') // tiles can make 'freedom' Not enough space to make 'do' or 'doorway'
+        val currentTiles = listOf('f','x', 'x', 'x', 'm', 'x') // tiles can make 'freedom' Not enough space to make 'do' or 'doorway'
         val output = underTest.findViableNextLetters(currentWord, currentTiles)
         assertNull(output)
 
@@ -107,7 +119,7 @@ class MyTest : ViewsForTesting() {
     @Test
     fun testGetNewViableTile_wordNotInWordsDictionary_throwException() {
         val currentWord = "sfsdfvsdfvsfvsvf"
-        val currentTiles = setOf('f','x', 'x', 'x', 'm', 'x') // tiles can make 'freedom' Not enough space to make 'do' or 'doorway'
+        val currentTiles = listOf('f','x', 'x', 'x', 'm', 'x') // tiles can make 'freedom' Not enough space to make 'do' or 'doorway'
         assertFailsWith<IllegalStateException> { // todo: make better exception
             underTest.findViableNextLetters(currentWord, currentTiles)
         }
